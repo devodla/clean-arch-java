26e804a9e682601b89ac579d2b111371ea151541
Huillca, Reynaldo <lymreynaldo@icloud.com>
2024-09-02 06:38:15 -0300

    chore: add init files

---
+++ src/main/java/uk/layme/clean_arch_java/CleanArchJavaApplication.java
@@ -0,0 +1,11 @@
+package uk.layme.clean_arch_java;
+
+import java.util.Arrays;
+
+public class CleanArchJavaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Init with args " + Arrays.toString(args));
+	}
+
+}
